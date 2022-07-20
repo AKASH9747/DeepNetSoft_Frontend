165301c6d0f0190ac57df39ee5f48f0c15c01cea
AKASH PS <psakash9747@gmail.com>
2022-07-20 12:25:51 +0530

    Design completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProductDetails from "./screens/ProductDetails";
+import ProductAddScreen from "./screens/ProductAddScreen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/product" element={<ProductDetails />} />
+          <Route path="/addproduct" element={<ProductAddScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
